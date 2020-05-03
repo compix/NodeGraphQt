78f07df694e5bc0f5648247b297873b1cd832cd9
compix <wladimirkroeker@googlemail.com>
2020-05-03 23:38:38 +0200

    zip nodes optional progress handler

--- node_exec/zip_nodes.py
+++ node_exec/zip_nodes.py
@@ -15,8 +15,17 @@ except:
     print("Note: Password-encryption for zip archives is not supported because the pyzipper module ist missing: pip install pyzipper")
 
 @defNode("Zip Folder", isExecutable=True, returnNames=["Zipped File Path"], identifier=IDENTIFIER)
-def zipFolder(folderPath="", outputZipFilePath="", password=""):
+def zipFolder(folderPath="", outputZipFilePath="", password="", progressHandler=None):
+    """
+    If a progressHandler is specified, the progress will be updated with 
+    progressHandler(progress, currentFileIndex, totalNumberOfFiles) where progress is a float in [0,1].
+    """
     relFolder = Path(folderPath).parent
+    
+    if progressHandler:
+        numberOfFiles = sum([len(files) for _, _, files in os.walk(folderPath)])
+        progressHandler(0.0, 0, numberOfFiles)
+        fileNumber = 0
     
     if password == "" or not pwEncryptionSupported:
         with ZipFile(outputZipFilePath, 'w') as zipFile:
@@ -24,6 +33,10 @@ def zipFolder(folderPath="", outputZipFilePath="", password=""):
                 for filename in filenames:
                     filePath = os.path.join(folderName, filename)
                     zipFile.write(filePath, os.path.relpath(filePath, relFolder))
+
+                    if progressHandler:
+                        fileNumber += 1
+                        progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
 
         return outputZipFilePath
     else:
@@ -34,14 +47,27 @@ def zipFolder(folderPath="", outputZipFilePath="", password=""):
                     filePath = os.path.join(folderName, filename)
                     zipFile.write(filePath, os.path.relpath(filePath, relFolder))
 
+                    if progressHandler:
+                        fileNumber += 1
+                        progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
+
 @defNode("Zip Files", isExecutable=True, returnNames=["Zipped File Path"], identifier=IDENTIFIER)
-def zipFiles(filePathList=None, outputZipFilePath="", keepRelativeFolderStructure=True, password=""):
+def zipFiles(filePathList=None, outputZipFilePath="", keepRelativeFolderStructure=True, password="", progressHandler=None):
+    """
+    If a progressHandler is specified, the progress will be updated with 
+    progressHandler(progress, currentFileIndex, totalNumberOfFiles) where progress is a float in [0,1].
+    """
     if filePathList == None:
         filePathList = []
 
     # Support single files:
     if not isinstance(filePathList, list):
         filePathList = [filePathList]
+
+    if progressHandler:
+        numberOfFiles = len(filePathList)
+        progressHandler(0.0, 0, numberOfFiles)
+        fileNumber = 0
 
     relFolder = Path(os.path.commonpath(filePathList)).parent
 
@@ -52,6 +78,10 @@ def zipFiles(filePathList=None, outputZipFilePath="", keepRelativeFolderStructur
                     zipFile.write(filePath, os.path.relpath(filePath, relFolder))
                 else:
                     zipFile.write(filePath, os.path.basename(filePath))
+
+                if progressHandler:
+                    fileNumber += 1
+                    progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
     else:
         with pyzipper.AESZipFile(outputZipFilePath, 'w', compression=pyzipper.ZIP_LZMA, encryption=pyzipper.WZ_AES) as zipFile:
             zipFile.pwd = password
@@ -61,16 +91,29 @@ def zipFiles(filePathList=None, outputZipFilePath="", keepRelativeFolderStructur
                 else:
                     zipFile.write(filePath, os.path.basename(filePath))
 
+                if progressHandler:
+                    fileNumber += 1
+                    progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
+
     return outputZipFilePath
 
 @defNode("Add Files to Zip", isExecutable=True, returnNames=["Zipped File Path"], identifier=IDENTIFIER)
-def addFilesToZip(filePathList=None, existingZipFile="", keepRelativeFolderStructure=True):
+def addFilesToZip(filePathList=None, existingZipFile="", keepRelativeFolderStructure=True, progressHandler=None):
+    """
+    If a progressHandler is specified, the progress will be updated with 
+    progressHandler(progress, currentFileIndex, totalNumberOfFiles) where progress is a float in [0,1].
+    """
     if filePathList == None:
         filePathList = []
 
     # Support single files:
     if not isinstance(filePathList, list):
         filePathList = [filePathList]
+
+    if progressHandler:
+        numberOfFiles = len(filePathList)
+        progressHandler(0.0, 0, numberOfFiles)
+        fileNumber = 0
 
     with ZipFile(existingZipFile, 'a') as zipFile:
         relFolder = Path(os.path.commonpath(filePathList)).parent
@@ -80,12 +123,25 @@ def addFilesToZip(filePathList=None, existingZipFile="", keepRelativeFolderStruc
                 zipFile.write(filePath, os.path.relpath(filePath, relFolder))
             else:
                 zipFile.write(filePath, os.path.basename(filePath))
-    
+
+            if progressHandler:
+                fileNumber += 1
+                progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
+
     return existingZipFile
 
 @defNode("Add Folder to Zip", isExecutable=True, returnNames=["Zipped File Path"], identifier=IDENTIFIER)
-def addFolderToZip(folderPath=None, existingZipFile=""):
+def addFolderToZip(folderPath=None, existingZipFile="", progressHandler=None):
+    """
+    If a progressHandler is specified, the progress will be updated with 
+    progressHandler(progress, currentFileIndex, totalNumberOfFiles) where progress is a float in [0,1].
+    """
     relFolder = Path(folderPath).parent
+
+    if progressHandler:
+        numberOfFiles = sum([len(files) for _, _, files in os.walk(folderPath)])
+        progressHandler(0.0, 0, numberOfFiles)
+        fileNumber = 0
 
     with ZipFile(existingZipFile, 'a') as zipFile:
         for folderName, _, filenames in os.walk(folderPath):
@@ -93,6 +149,10 @@ def addFolderToZip(folderPath=None, existingZipFile=""):
                 filePath = os.path.join(folderName, filename)
                 zipFile.write(filePath, os.path.relpath(filePath, relFolder))
 
+                if progressHandler:
+                    fileNumber += 1
+                    progressHandler(float(fileNumber) / numberOfFiles, fileNumber, numberOfFiles)
+                    
     return existingZipFile
 
 """
